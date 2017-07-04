dfadc76a200e3342da33b8f4af910d7af8def140
Kenneth Ingham <ingham@i-pi.com>
2017-07-04 15:01:26 -0600

    Threading working, checkpointing in progress

--- metric.h
+++ metric.h
@@ -11,7 +11,7 @@ class metric {
 	virtual void printdetails() {};
 
 	// needs to be implemented in subclasses; -1 is illegal for a distance.
-	virtual long double compare(const FastaRecord& a, const FastaRecord& b) {return -1.0;};
+	virtual long double compare(const FastaRecord& a, const FastaRecord& b) const {return -1.0;};
 
 	// Not needed?
 	//virtual metric& operator=(const metric &c) {};

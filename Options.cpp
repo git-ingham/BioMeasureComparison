f9198e61b1f427224e33f6314ac464019ea29076
Kenneth Ingham <ingham@i-pi.com>
2017-09-11 09:46:20 -0600

    Fixed overzealous validation

--- Options.cpp
+++ Options.cpp
@@ -45,7 +45,7 @@ Options::Options(int argc, char **argv)
     option_defs[findoption("measure")].checksanity = validatemetric;
     option_defs[findoption("submeasure")].checksanity = novalidation;
     option_defs[findoption("measureopt")].checksanity = novalidation;
-    option_defs[findoption("distmatfname")].checksanity = validatefile;
+    option_defs[findoption("distmatfname")].checksanity = novalidation;
     option_defs[findoption("ncores")].checksanity = validatecores;
     option_defs[findoption("checkpointdir")].checksanity = validatedir;
 

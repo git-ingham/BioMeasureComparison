3bbb9471596ada423d62f797f81515f075d40252
Kenneth Ingham <ingham@i-pi.com>
2018-03-05 11:10:43 -0700

    Test driver for deBruijnGraph code

---
+++ testdebruijn.cpp
@@ -0,0 +1,13 @@
+#include "deBruijnGraph.h"
+#include <iostream>
+
+int main()
+{
+    for (unsigned int k=2; k<16; ++k) {
+        std::cout << "Making a graph of size " << k << std::endl;
+        deBruijnGraph* db = new deBruijnGraph(k);
+        db->make_complete_graph();
+        db->consistency_check();
+        delete db;
+    }
+}

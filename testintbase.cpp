386d05f0a7f638ea023156d4e792e5e6151a8f9b
Kenneth Ingham <ingham@i-pi.com>
2018-04-16 14:10:34 -0600

    Updates for refactored code

--- testintbase.cpp
+++ testintbase.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 #include "intbase.h"
+#include <log4cxx/logger.h>
+#include <log4cxx/basicconfigurator.h>
 
 int main()
 {
+    log4cxx::BasicConfigurator::configure();
+
     intbase ib;
     ib.test();
     std::cout << "All tests completed successfully." << std::endl;
--- testkmerint.cpp
+++ testkmerint.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
+#include <log4cxx/logger.h>
+#include <log4cxx/basicconfigurator.h>
+
 #include "kmerint.h"
 #include "intbase.h"
 
 int main()
 {
-    for (unsigned int k=min_k; k<=max_k; ++k) {
+    log4cxx::BasicConfigurator::configure();
+
+    for (unsigned int k=kmer::min_k; k<=kmer::max_k; ++k) {
         std::cout << "testing k = " << k << std::endl;
         kmerint* kmer = new kmerint(k);
-        kmer->test(false);
+        kmer->test(true);
         delete kmer;
     }
 }

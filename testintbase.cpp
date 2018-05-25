5624474187cfc4e6fba94ed58b968a1adb7ed323
Kenneth Ingham <ingham@i-pi.com>
2018-05-25 14:15:53 -0600

    Updated to know about and test intbaseDNA and intbase2

--- testintbase.cpp
+++ testintbase.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include "intbase.h"
+#include "intbaseDNA.h"
+#include "intbase2.h"
 #include <log4cxx/logger.h>
 #include <log4cxx/basicconfigurator.h>
 
@@ -7,7 +8,17 @@ int main()
 {
     log4cxx::BasicConfigurator::configure();
 
-    intbase ib;
-    ib.test();
-    std::cout << "All tests completed successfully." << std::endl;
+    intbaseDNA ibdna;
+    test_intbase(ibdna);
+    test_intbaseDNA(ibdna);
+    std::cout << "All intbaseDNA tests completed successfully." << std::endl;
+
+    intbase2 ib2;
+    test_intbase(ib2);
+    test_intbase2(ib2);
+    std::cout << "All intbase2 tests completed successfully." << std::endl;
+
+    // uncomment to test necessity of subclassing
+//     intbase ib;
+//     test_intbase(ib); // should die since tests must be on a subclass
 }
